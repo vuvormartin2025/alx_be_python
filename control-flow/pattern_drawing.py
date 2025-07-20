37c96b74e04ba792a64644a17b47d751dae25107
vuvormartin2025 <vuvormartin@gmail.com>
2025-07-20 20:05:13 +0000

    Updated pattern_drawing.py with changes

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,21 +1,12 @@
-while True:
-    try:
-
-        size = int(input("Enter the size of pattern:"))
-        if size > 0:
-            break
-        else:
-            print("please enter a positive integer greater than 0.")
-    except ValueError:
-        print("Invalid input. Please enter a valid positive integer .")
-
+size = int(input("Enter the size of pattern:"))
+        
 row = 0
 
 # Outer loop - while loop for rows
 
 while row < size:
 
-    for col in range(size):
+    for x in range(size):
         print("*", end="")
 
     print()
